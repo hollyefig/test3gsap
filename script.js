1ae08f0f7fbb175c8dabf5bac489edbf2333821f
mts_dev3 <mts_dev3@stephensdirect.com>
2023-10-12 10:18:07 -0400

    optimizing animation properties

--- script.js
+++ script.js
@@ -1,9 +1,7 @@
 const size = { width: 600, height: 250 },
   rotateIn = { fontSize: "1rem", rotation: 45, opacity: 0 },
   thirdSlide = document.querySelector(".s3sq1"),
-  thirdSlideText = "I like to eat cheese!".split("");
-
-let spanArray = [];
+  thirdSlideText = "I always choose Charmander!".split("");
 
 for (let i = 0; i < thirdSlideText.length; i++) {
   const span = document.createElement("span");
@@ -12,29 +10,7 @@ for (let i = 0; i < thirdSlideText.length; i++) {
     ? (span.textContent = "\u00A0")
     : (span.textContent = thirdSlideText[i]);
   thirdSlide.appendChild(span);
-  spanArray.push(span);
 }
-
-const setFunc = (a, onCompleteCallback) => {
-  let int1 = 50;
-
-  gsap.to(a, {
-    opacity: 1,
-    duration: 0.2,
-    ease: "power2.inOut",
-    stagger: int1 / 1000,
-    onComplete: () => {
-      gsap.to(a, {
-        color: "#0DBBDE",
-        duration: 0.2,
-        onComplete: onCompleteCallback,
-        // Call the onCompleteCallback when
-        // this animation is complete
-        stagger: int1 / 1000,
-      });
-    },
-  });
-};
 
 let timeline = gsap.timeline({
   defaults: {
@@ -45,33 +21,40 @@ let timeline = gsap.timeline({
 });
 
 timeline
-  .from(".sq1", { y: size.height * -1, delay: 2 })
-  .from(".sq2", { y: size.height * -1 }, "<.3")
-  .from(".sq3", { y: size.height * -1 }, "<.3")
-  .from(".sq1 h2", rotateIn, "<")
+  .from(".slide1 > div", { y: size.height * -1, stagger: 0.3, delay: 1 })
+  .from(".sq1 h2", rotateIn, "<.5")
   .from(".sq1 .material-symbols-outlined", rotateIn, "<.2")
   .from(".sq2 h2", rotateIn, "<")
   .from(".sq2 .material-symbols-outlined", rotateIn, "<.2")
   .from(".sq3 h2", rotateIn, "<")
   .from(".sq3 .material-symbols-outlined", rotateIn, "<.2")
-  .to(".slide2", { y: size.height * -1, duration: 0.8, delay: 2.5 }, "<")
-  .from(".s2sq1", { x: size.width, duration: 0.8 }, "<.5")
-  .from(".s2sq2", { x: size.width, duration: 0.8 }, "<.2")
-  .from(".s2sq3", { x: size.width, duration: 0.8 }, "<.2")
+  .to(".slide2", { duration: 0.8, delay: 2.5 }, "<")
+  .from(".slide2 > div", { x: size.width, duration: 0.8, stagger: 0.2 }, "<.5")
   .to(".slide1 > div > div", { opacity: 0 })
-  .to(".s2sq1b", { x: -200, duration: 0.8, delay: 2 })
-  .to(".s2sq2b", { x: -200, duration: 0.8 }, "<.2")
-  .to(".s2sq3b", { x: -200, duration: 0.8 }, "<.2")
+  .to(".s2sq1b, .s2sq2b, .s2sq3b", {
+    x: -200,
+    duration: 0.8,
+    delay: 2,
+    stagger: 0.2,
+  })
   .to(".s2sq1, .s2sq3", { delay: 3, y: size.height })
   .to(".s2sq2", { y: size.height * -1 }, "<")
   .to(".sq1, .sq3", { y: size.height }, "<.2")
   .to(".sq2", { y: size.height * -1 }, "<")
-  .add(() =>
-    setFunc(spanArray, () => {
-      // This function will be called when the
-      // setFunc animation is complete.
-      timeline
-        .to(".slide3", { backgroundColor: "#0dbbde" })
-        .to(".s3sq1 span", { color: "#fff" }, "<");
-    })
-  );
+  .to(".s3sq1 span", {
+    opacity: 1,
+    duration: 0.2,
+    ease: "power2.inOut",
+    stagger: 0.05,
+  })
+  .to(
+    ".s3sq1 span",
+    {
+      color: "#0DBBDE",
+      duration: 0.2,
+      stagger: 0.05,
+    },
+    "<.5"
+  )
+  .to(".slide3", { backgroundColor: "#0dbbde" })
+  .to(".s3sq1 span", { color: "#fff" }, "<");
